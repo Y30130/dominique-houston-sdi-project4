7710c76406992ce79d752c130fc59e743a564abc
Dominique Houston <dominiquehouston@gmail.com>
2013-05-30 14:43:28 -0600

    Cleaned Up US Phone Number String

--- dominique-houston-sdi-project4.js
+++ dominique-houston-sdi-project4.js
@@ -1,13 +1,31 @@
 // alert("JavaScript works!");
+// Dominique Houston
+// SDI 1305
+// Project 4
+// Javascript Libraries
+
 // String Library
 var StringLibrary = function () {
-
 	// Does a string follow a 123-456-7890 pattern like a phone number?
 	// Return boolean
 	var isPhoneNumUS = function (str) {
 		var phoneNum= new String("123-456-7890");
-		var re = /\d{3}-\d{3}-\d{4}/;
-		return re.test(str);
+		var result = (/\d{3}-\d{3}-\d{4}/);
+		return result.test(str);
 	};
+	
+	
+	
+	return {
+			"isPhoneNumUS" : isPhoneNumUS, // Return Boolean Result from phone number string test
+			
+		};
 
 };
+
+
+// String Tests
+console.log("Strings Library Execution");
+console.log("");
+var stringLib = StringLibrary(); // Execute String Library
+console.log("Phone Number (US): " + stringLib.isPhoneNumUS("404-987-5564"));
