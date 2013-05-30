15d89aa17c3562dff02cdf1e23e3fa59efc2f9ef
Dominique Houston <dominiquehouston@gmail.com>
2013-05-30 14:50:17 -0600

    Email Address String

--- dominique-houston-sdi-project4.js
+++ dominique-houston-sdi-project4.js
@@ -7,17 +7,24 @@
 // String Library
 var StringLibrary = function () {
 	// Does a string follow a 123-456-7890 pattern like a phone number?
-	// Return boolean
+	// Return boolean value
 	var isPhoneNumUS = function (str) {
 		var phoneNum= new String("123-456-7890");
-		var result = (/\d{3}-\d{3}-\d{4}/);
+		var result = (/\d{3}-\d{3}-\d{4}/); // Regular Expression, all resources lead to this conclusion			
 		return result.test(str);
 	};
 	
+	// Does a string follow an aaa@bbb.ccc pattern like an email address?
+	// Return boolean value
+	var isEmail = function (str) {
+		var re = /^\w+@[\w.\-]+\.[A-Za-z]{2,}$/; // Regular Expression, all resources lead to this conclusion
+		return re.test(str);
+	};
 	
 	
 	return {
 			"isPhoneNumUS" : isPhoneNumUS, // Return Boolean Result from phone number string test
+			"isEmail"      : isEmail, // Return Boolean Reult from email format test
 			
 		};
 
@@ -25,7 +32,9 @@ var StringLibrary = function () {
 
 
 // String Tests
-console.log("Strings Library Execution");
-console.log("");
+console.log("STRING LIBRARY EXECUTION");
 var stringLib = StringLibrary(); // Execute String Library
+console.log("");
+console.log("Test Results from String Library Function");
 console.log("Phone Number (US): " + stringLib.isPhoneNumUS("404-987-5564"));
+console.log("E-mail Address: "    + stringLib.isEmail("dominique@dominiquehouston.com"));
