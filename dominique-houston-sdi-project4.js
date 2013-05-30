210b71c1fef245bae8e3ac5d9efa038c8621aeb5
Dominique Houston <dominiquehouston@gmail.com>
2013-05-30 14:31:42 -0600

    String Library Set up - Us Phone Number String Validation

--- dominique-houston-sdi-project4.js
+++ dominique-houston-sdi-project4.js
@@ -1,1 +1,13 @@
 // alert("JavaScript works!");
+// String Library
+var StringLibrary = function () {
+
+	// Does a string follow a 123-456-7890 pattern like a phone number?
+	// Return boolean
+	var isPhoneNumUS = function (str) {
+		var phoneNum= new String("123-456-7890");
+		var re = /\d{3}-\d{3}-\d{4}/;
+		return re.test(str);
+	};
+
+};
