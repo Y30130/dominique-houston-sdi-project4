cc478c9714b470c23fd16f0367cb919d5cee666d
Dominique Houston <dominiquehouston@gmail.com>
2013-05-30 16:57:43 -0600

    Code Final Release
    Corrected HTML pages, and finished JavaScript code.

--- dominique-houston-sdi-project4.js
+++ dominique-houston-sdi-project4.js
@@ -1,4 +1,5 @@
 // alert("JavaScript works!");
+
 // Dominique Houston
 // SDI 1305
 // Project 4
@@ -96,7 +97,7 @@ var NumberLibrary = function () {
 // Array Library
 var ArrayLibrary = function () {
 
-	// Find the smallest value in an array that is greater than a given number
+	// Find the smallest value in an array that is greater than a given number, if no value, return null
 	var smallestValueInArray = function (array,num) {
 		array.sort(function(a,b){return a-b;});
 		if (num >= array[0] && num < array[array.length-1]) {
@@ -109,10 +110,28 @@ var ArrayLibrary = function () {
 		}
 	}
 	
+	// Find the total value of just the numbers in an array, even if some of the items are not numbers
+	var totalValNumInArray = function (array) {
+		var total = 0;
+		for (var i = 0, j = array.length; i < j; i++) {
+				if (array[i]/1 === array[i]) {
+					total += array[i];
+				};
+		};
+		return total;
+	}
+	
+	// Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}]
+	var sortKeyByValInArray = function (array,givenKey) {
+		return (array.sort(function(a,b){return a[givenKey] - b[givenKey];}));
+	};
+	
 	
 	return {
 		
 		"smallestValueInArray" : smallestValueInArray,
+		"totalValNumInArray" : totalValNumInArray,
+		"sortKeyByValInArray": sortKeyByValInArray,
 	}
 	
 	
@@ -123,12 +142,11 @@ console.log("STRING LIBRARY EXECUTION");
 var stringLib = StringLibrary(); // Execute String Library
 console.log("");
 console.log("Test Results from String Library Function");
-console.log("Phone Number (US): " + stringLib.isPhoneNumUS("404-987-5564"));
-console.log("E-mail Address: "    + stringLib.isEmail("dominique@dominiquehouston.com"));
-console.log("URL: "    + stringLib.isURL("http://dominiquehouston.com/"));
-console.log("Title-case: " + stringLib.splitStrUppercase("britney jean spears was here"));
-console.log("Separator Switch: " + stringLib.switchSeparator("dont stop and make it pop","/"));
-
+console.log("Phone Number (US): " + stringLib.isPhoneNumUS("404-987-5564")); 	// Does a string follow a 123-456-7890 pattern like a phone number?
+console.log("E-mail Address: "    + stringLib.isEmail("dominique@dominiquehouston.com")); 	// Does a string follow an aaa@bbb.ccc pattern like an email address?
+console.log("URL: "    + stringLib.isURL("http://dominiquehouston.com/"));	// Is the string a URL? (Does it start with http: or https:?)
+console.log("Title-case: " + stringLib.splitStrUppercase("britney jean spears was here"));	// Title-case a string (split into words, then uppercase the first letter of each word)
+console.log("Separator Switch: " + stringLib.switchSeparator("dont stop and make it pop","/"));	// Given a string that is a list of things separated by a given string, as well as another string separator, 
 console.log("");
 console.log("");
 
@@ -137,13 +155,13 @@ console.log("NUMBER LIBRARY EXECUTION");
 var numberLib = NumberLibrary(); // Execute String Library
 console.log("");
 console.log("Test Results from String Library Function");
-console.log("Format Number: " + numberLib.formatNumto2Dec(1.2345,2));
-var date1 = new Date(1990,1,26);
+console.log("Format Number: " + numberLib.formatNumto2Dec(1.2345,2));	// Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+var date1 = new Date(2013,1,26);
 var date2 = new Date(2013,5,30);
-var timeConversion = numberLib.compareTimeBtDates(date1,date2);
-console.log("Difference in days: " + timeConversion[0] + ", in hours: " + timeConversion[1]);
-console.log("Blue " + numberLib.strToNum("42") + "!");
-console.log(" ");
+var timeConversion = numberLib.compareTimeBtDates(date1,date2);	// Find the number of hours or days difference between two dates.
+console.log("Difference in days: " + timeConversion[0] + ", in hours: " + timeConversion[1]); // Difference Output
+console.log("String Conversion: " + "Blue " + numberLib.strToNum("42") + "!"); 	// Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+console.log("");
 console.log("");
 
 // Array Tests
@@ -152,4 +170,8 @@ var arrayLib = ArrayLibrary(); // Execute String Library
 console.log("");
 console.log("Test Results from Array Library Function");
 var numList = [11,1,90,10,12,2,4,8,5,];
-console.log(arrayLib.smallestValueInArray(numList,2));
+console.log("Array: " + numList);
+console.log("Smallest value in array: " + arrayLib.smallestValueInArray(numList,2)); 	// Find the smallest value in an array that is greater than a given number
+console.log("Sum of array: " + arrayLib.totalValNumInArray(numList)); 	// Find the total value of just the numbers in an array, even if some of the items are not numbers
+var arrayObjects = [{a:4},{a:2},{a:3},{a:87},{a:3},{a:1},{a:4},{a:7},{b:1},{b:5}];
+console.log(arrayLib.sortKeyByValInArray(arrayObjects,"a")); // Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a"
